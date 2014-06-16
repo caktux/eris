8a6e3febcdf18c5e05a3a1d7a767415ec7ee7bc0
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-06-16 06:24:42 +0200

    marker for get_values function on VotePost action

--- bylaws/vote_post.rb
+++ bylaws/vote_post.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 class VotePost
+  attr_reader :vote_count
 
   def initialize post_id, up_or_down, up_down_vote_bylaw
     if up_or_down == 'upvote' or up_or_down == 'downvote'
       if post_id
         build_transaction post_id, up_or_down, up_down_vote_bylaw
+        get_values post_id
       end
     end
   end
@@ -15,5 +17,9 @@ class VotePost
     def build_transaction post_id, up_or_down, up_down_vote_bylaw
       data      = [ up_or_down, post_id ]
       $eth.transact up_down_vote_bylaw, data
+    end
+
+    def get_values post_id
+      @vote_count = 0 #TODO - fix
     end
 end
