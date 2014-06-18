fe0c46d99d47c028ab33716496c0ff8c54a9b20a
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-06-18 14:36:22 +0200

    clean out unneccessary js

--- app/assets/scripts/application.js
+++ app/assets/scripts/application.js
@@ -1,7 +1,2 @@
-// require ./underscore/underscore
 //= require ./jquery/dist/jquery.min
 //= require ./bootstrap/dist/js/bootstrap.min
-// require ./knockout.js/knockout
-// require ./knockout-bootstrap/build/knockout-bootstrap.min
-// require ./eris-models
-// require ./eris-view-model
--- app/assets/scripts/eris-view-model.js
+++
@@ -1,7 +0,0 @@
-// View Model for Eris
-function ErisServerViewModel() {
-
-};
-
-window.eris = new ErisServerViewModel();
-ko.applyBindings( eris );
